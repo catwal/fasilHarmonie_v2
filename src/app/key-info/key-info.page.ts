433e75ec472e6f0b9ec473d27c57cf6d17c0ccea
catwal <kitekate.cwgmail.com>
2019-07-22 18:14:10 +0200

    start key-info picker

--- src/app/key-info/key-info.page.ts
+++ src/app/key-info/key-info.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PickerController } from '@ionic/angular';
+import { PickerOptions, PickerButton } from '@ionic/core';
 
 @Component({
   selector: 'app-key-info',
@@ -7,23 +9,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KeyInfoPage implements OnInit {
 
-  public index = [
-    'DO',
-    'DO#',
-    'RE',
-    'RE#',
-    'MI',
-    'FA',
-    'FA#',
-    'SOL',
-    'SOL#',
-    'LA',
-    'LA#',
-    'SI'
-  ];
-  constructor() { }
+
+  key = '';
+  tone = '';
+  constructor(private pickerCtrl: PickerController) { }
 
   ngOnInit() {
   }
 
+  async showKeys() {
+    const opts: PickerOptions = {
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        },
+        {
+          text: 'Done'
+        }
+      ],
+      columns: [
+        {
+          name: 'key',
+          options: [
+            {text: 'DO', value: 'do'},
+            {text: 'DO#', value: 'do#'},
+            {text: 'RE', value: 're'},
+            {text: 'RE#', value: 're#'},
+            {text: 'MI', value: 'mi'},
+            {text: 'FA', value: 'fa'},
+            {text: 'SOL', value: 'sol'},
+            {text: 'SOL#', value: 'sol#'},
+
+          ]
+        }
+      ]
+    };
+
+  // public index = [
+  //     'DO',
+  //     'DO#',
+  //     'RE',
+  //     'RE#',
+  //     'MI',
+  //     'FA',
+  //     'FA#',
+  //     'SOL',
+  //     'SOL#',
+  //     'LA',
+  //     'LA#',
+  //     'SI'
+  //   ];
+  }
 }
